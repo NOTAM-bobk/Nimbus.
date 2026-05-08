2eba91407ff1edf0aba3bb41f3be30f4694b9fea
NOTAM <sawyerbobk563@gmail.com>
2026-05-08 10:45:22 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,16 @@
+self.addEventListener('push', (event) => {
+    const data = event.data ? event.data.text() : 'New Notification!';
+    
+    event.waitUntil(
+        self.registration.showNotification('Nimbus', {
+            body: data,
+            icon: '/icon-192.png',
+            badge: '/icon-192.png'
+        })
+    );
+});
 
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    event.waitUntil(clients.openWindow('/'));
+});
